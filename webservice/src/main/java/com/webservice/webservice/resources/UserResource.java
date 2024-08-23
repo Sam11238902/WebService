7058d6abe4c99d5b9c037362bc3e130381166f00
Samuel Holanda <samuel112928@gmail.com>
2024-08-23 15:55:27 -0300

    adicionando a entidade user

---
+++ webservice/src/main/java/com/webservice/webservice/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.webservice.webservice.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.webservice.webservice.entities.User;
+
+@RestController
+
+@RequestMapping(value ="/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+	
+		User u = new User(1L, "Samuel", "samuel@gmail.com", "85986309782", "sasasa123");
+		
+		return ResponseEntity.ok().body(u);
+		
+	}
+
+}
