6452eadb87879974aa52d620b3459267c01a697b
Samuel Holanda <samuel112928@gmail.com>
2024-08-23 19:17:40 -0300

    adicionando Order , instant, ManytoOne/OneToMany

--- webservice/src/main/java/com/webservice/webservice/entities/User.java
+++ webservice/src/main/java/com/webservice/webservice/entities/User.java
@@ -1,12 +1,16 @@
 package com.webservice.webservice.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -23,6 +27,14 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")		
+	private List<Order> orders = new ArrayList<>();
+	
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	public User () {
 		
 	}
--- webservice/src/main/java/com/webservice/webservice/resources/UserResource.java
+++ webservice/src/main/java/com/webservice/webservice/resources/UserResource.java
@@ -1,29 +1,45 @@
 package com.webservice.webservice.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webservice.webservice.entities.User;
+import com.webservice.webservice.service.UserService;
 
 @RestController
 
 @RequestMapping(value ="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
 	
 	
 	//PARA FINS DE TESTES O METODO ABAIXO ;
 	@GetMapping
-	public ResponseEntity<User> findAll(){
+	public ResponseEntity<List<User>> findAll(){
 	
-		User u = new User(1L, "Samuel", "samuel@gmail.com", "85986309782", "sasasa123");
+		//User u = new User(1L, "Samuel", "samuel@gmail.com", "85986309782", "sasasa123");
 		
 		
+		List<User> users = service.findAll();
 		
+		return ResponseEntity.ok().body(users);
 		
-		return ResponseEntity.ok().body(u);
+	}
+	
+	
+	@GetMapping(value ="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 		
 	}
 
